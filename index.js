11dd29aa96bb85138c5f21a8e299ecc38398cd86
Fawnthefox <48794324+Fawnthefox@users.noreply.github.com>
2019-03-21 19:36:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const client = new Discord.Client({disableEveryone: true}); //Make a new cilent 
 let config = require('./config.json'); //Register config Directory
 const prefix = config.PREFIX; //Register Prefix
 
+client.on('ready', () => { //When The Bot Is Ready
+  console.log(`Logged in as ${client.user.tag}!`); //Log The Cilent Username To The Console
+});
+
 client.on('message', message => { //When There Is A Message
 if(message.content === prefix + 'hi') { //Check if message is prefix + hi
 if(message.author.bot) return; //If the user is a bot then return
@@ -13,3 +17,4 @@ message.channel.send('Hello!') //Send Message To That Channel
 client.login(config.BOT_TOKEN); //Login To The Bot Token
 
 //More Thing That This Can Do On This Website https://discord.js.org/#/docs/main/stable/general/welcome
+//If you want to make the command owner only put this in if(message.author.id != config.OWNER_ID) return;
