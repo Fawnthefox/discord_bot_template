fd5cd9e2285594d685906f4fd8143062a55642fa
Fawnthefox <48794324+Fawnthefox@users.noreply.github.com>
2019-03-21 19:16:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js'); //Extract Discord.js Module
+const client = new Discord.Client({disableEveryone: true}); //Make a new cilent also disable @everyone
+let config = require('./config.json'); //Register config Directory
+const prefix = config.PREFIX; //Register Prefix
+
+client.on('message', message => { //When There Is A Message
+if(message.content === prefix + 'hi') { //Check if message is prefix + hi
+message.channel.send('Hello!') //Send Message To That Channel
+}})
+
+client.login(config.BOT_TOKEN); //Login To The Bot Token
+
+//More Thing That This Can Do On This Website https://discord.js.org/#/docs/main/stable/general/welcome
